72c62743ee9499b2d6825bcbeb4a53933473d9e9
H Brooke <hbrooke36@yahoo.com>
2022-11-12 12:12:17 -0800

    Add form functionality, book card generation

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
+let bookTitle;
+let bookAuthor;
+let bookPagesRead;
+
 function addListeners(){
-    const div = document.querySelectorAll('.button'); 
-    div => div.addEventListener('click', addBook);
+    console.log("adding listeners")
+    const btn = document.querySelectorAll('.button'); 
+    console.log(btn);
+    btn[0].addEventListener('click', bookFormSubmit);
+}
+
+function bookFormSubmit(){
+    console.log("been pressed")
+    bookTitle = document.getElementById('book-title').value;
+    bookAuthor = document.getElementById('author').value;
+    bookPagesRead = document.getElementById('pages-read').value;
+    addBook(bookTitle, bookAuthor, bookPagesRead);
+}
+
+function addBook(title, author, pagesRead){
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('book');
+    console.log(card);
+    const shelf = document.getElementsByClassName('shelf');
+    shelf[0].appendChild(card);
+    const info = document.createElement('div');
+    info.classList.add('book-info');
+    card.appendChild(info);
+    const bktitle = document.createElement('div');
+    bktitle.classList.add('book-title');
+    info.appendChild(bktitle);
+    bktitle.textContent = bookTitle;
+    const bkauthor = document.createElement('div');
+    bkauthor.classList.add('author');
+    info.appendChild(bkauthor);
+    bkauthor.textContent = bookAuthor;
+    const bkpagesRead = document.createElement('div');
+    bkpagesRead.classList.add('pages-read');
+    info.appendChild(bkpagesRead);
+    bkpagesRead.textContent = bookPagesRead;
 }
 
 addListeners();
-
-function addBook(e){ 
-    let num = e.target.innerHTML;
-    lastButtonPressed = num;
-    let display = document.getElementById('display');  
-}
