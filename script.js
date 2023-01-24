ac5d54f9748dfadde1dd7ab6ded89e931363cb89
HeidiBrooke <hbrooke36@yahoo.com>
2023-01-23 18:16:39 -0800

    Add custom form validation

--- script.js
+++ script.js
@@ -192,3 +192,78 @@ function closeForm(){
 
 displayBooks1();
 addListeners();
+
+
+//form validation
+
+const bookTitleElement = document.getElementById('book-title');
+bookTitleElement.addEventListener('input', checkBookValidity);
+
+const bookAuthorElement = document.getElementById('author');
+bookAuthorElement.addEventListener('input', checkAuthorValidity);
+
+const bookPagesElement = document.getElementById('pages-read');
+bookPagesElement.addEventListener('input', checkPagesValidity);
+
+function checkBookValidity(e) {
+    console.log('checking validity')
+    let error;
+    if(!e.target.checkValidity()){
+        error = document.createElement('span');
+        error.classList.add('error');
+        error.setAttribute('id', 'book-error');
+        error.textContent = 'A title is required.'
+        e.target.parentElement.appendChild(error);
+    }
+    else{
+        error = document.getElementById('book-error')
+        if(error !== null){
+            error.remove();
+        }
+    }
+}
+
+function checkAuthorValidity(e) {
+    console.log('checking validity')
+    let error;
+    if(!e.target.checkValidity()){
+        error = document.getElementById('author-error');
+        if(error === null){
+            error = document.createElement('span');
+            error.classList.add('error');
+            error.setAttribute('id', 'author-error');
+            error.textContent = 'Author must contain at least 3 characters'
+            e.target.parentElement.appendChild(error);
+        }
+        
+    }
+    else{
+        error = document.getElementById('author-error')
+        if(error !== null){
+            error.remove();
+        }
+    }
+}
+
+function checkPagesValidity(e) {
+    console.log('checking validity')
+    let error;
+    if(!e.target.checkValidity()){
+        error = document.getElementById('page-error');
+        if(error === null){
+            error = document.createElement('span');
+            error.classList.add('error');
+            error.setAttribute('id', 'page-error');
+            error.textContent = 'Entry must be a number'
+            e.target.parentElement.appendChild(error);
+        }
+        
+    }
+    else{
+        error = document.getElementById('page-error')
+        if(error !== null){
+            error.remove();
+        }
+    }
+}
+
